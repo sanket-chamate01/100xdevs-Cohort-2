09cd5c6921704abb8fa08fb7488951742c2fd4e6
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-11-23 14:26:31 +0530

    added gitignore file

--- Practice/Week 4/sum and interest app/app.js
+++ Practice/Week 4/sum and interest app/app.js
@@ -11,7 +11,12 @@ app.get("/sum", (req, res) => {
 })
 
 app.get("/interest", (req, res) => {
-    
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const c = parseInt(req.query.c);
+    const interest = a * (b/100) * c;
+    const total = interest + a;
+    res.send(`Interest = ${interest}<br>Total Amount to be paid = ${total}`);
 })
 
 app.listen(3000, () => {
