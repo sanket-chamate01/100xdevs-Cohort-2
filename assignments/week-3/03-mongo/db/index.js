e39ce6c4af3b13508d6a424349e1121cf6d71e7f
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-11-27 16:07:49 +0530

    mongo assignment started

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -6,6 +6,7 @@ mongoose.connect('your-mongodb-url');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    name: String,
 });
 
 const UserSchema = new mongoose.Schema({
