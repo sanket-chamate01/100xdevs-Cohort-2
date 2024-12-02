dc3d83900cc439a3b75f7201b5c235a1702b6625
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-12-02 20:27:23 +0530

    week 6 started, React.memo

---
+++ Practice/Week 6/experiments/src/App.jsx
@@ -0,0 +1,66 @@
+import { memo, useState } from "react"
+
+// memo does not let children re-render if they are not updated
+
+function App() {
+
+  const [title, titleState] = useState("My name is Sanket")
+
+  function changeTitle(){
+    titleState(`My name is ${Math.random()}`)
+  }
+
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={title}></Header>
+      <Header title="My name is Sanket"></Header>
+      <Header title="My middlename is Dilip"></Header>
+      <Header title="My lastname is Chamate"></Header>
+    </div>
+  )
+
+  // without memo
+
+  // return ( 
+  //   <>
+  //     <HeaderWithButton></HeaderWithButton>
+  //     <Header title="My name is Sanket"></Header>
+  //   </>
+  // )
+}
+
+function HeaderWithButton(){ // without memo
+  const [title, titleState] = useState("My name is Sanket")
+
+  function changeTitle(){
+    titleState(`My name is ${Math.random()}`)
+  }
+
+  return (
+    <div>
+      <button onClick={changeTitle}>Click me to change the title</button>
+      <Header title={title}></Header>
+    </div>
+  )
+}
+
+const Header = memo(function Header({title}){ // React.memo
+  return(
+    <div>
+      {title}
+    </div>
+  )
+})
+
+// without memo
+
+// function Header({title}){
+//   return(
+//     <div>
+//       {title}
+//     </div>
+//   )
+// }
+
+export default App
