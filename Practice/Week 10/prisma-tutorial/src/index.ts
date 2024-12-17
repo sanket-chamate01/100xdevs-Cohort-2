1b740be71892af71664d50ada482d8905ef6abf5
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-12-17 19:00:03 +0530

    prisma delete function done

--- Practice/Week 10/prisma-tutorial/src/index.ts
+++ Practice/Week 10/prisma-tutorial/src/index.ts
@@ -1,12 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 
+// if we write wrong wueries or commands and there is error then also id increments that's why riyu get id = 3
+
 const prisma = new PrismaClient()
 
 // insertUser("sanket@gmail.com", "123456", "sanket", "chamate")
 // insertUser("riyu@gmail.com", "123456", "riyu", "bajaj")
 // insertUser("aryan@gmail.com", "123456", "aryan", "roy")
+// insertUser("aryan2@gmail.com", "123456", "aryan2", "roy")
 
 // updateUser("riyu@gmail.com", {password: "852460"})
+
+// deleteUser("aryan2@gmail.com")
 
 async function insertUser(username: string, password: string, firstname: string, lastname: string){
     const res = await prisma.user.create({
@@ -35,6 +40,15 @@ async function updateUser(username: string, value : UpdateParams){ // or {firstn
             firstname: value.firstname,
             lastname: value.lastname,
             password: value.password
+        }
+    })
+    console.log(res)
+}
+
+async function deleteUser(username: string){
+    const res = await prisma.user.delete({
+        where: {
+            username
         }
     })
     console.log(res)
