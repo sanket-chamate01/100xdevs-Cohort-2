fe45957adb7878b9c2d5900a6651230db32e594a
sanket-chamate01 <sanket.chamate2001@gmail.com>
2024-12-23 14:22:58 +0530

    postgres-relationship todo function done

---
+++ Practice/Week 12/prisma-relationship/prisma/migrations/20241223061733_user_todo_relationship/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todos" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "done" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todos_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Todos" ADD CONSTRAINT "Todos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- Practice/Week 12/prisma-relationship/src/index.ts
+++ Practice/Week 12/prisma-relationship/src/index.ts
@@ -2,7 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient()
 
-async function insertTodo(username: string, password: string, firstname: string, lastname: string){
+// -------------------------------------------------------------- User --------------------------------------------------------------
+
+// insertUser("sanket@gmail.com", "123456", "sanket", "chamate")
+// insertUser("riyu@gmail.com", "123456", "riyu", "sharma")
+// insertUser("aryan@gmail.com", "123456", "aryan", "roy")
+// insertUser("parth@gmail.com", "123456", "parth", "bajaj")
+
+
+async function insertUser(username: string, password: string, firstname: string, lastname: string){
     const res = await prisma.user.create({
         data: {
             username,
@@ -20,7 +28,7 @@ interface UpdateParams {
     password?: string
 }
 
-async function updateTodo(username: string, value : UpdateParams){ // or {firstname, lastname, password} : UpdateParams
+async function updateUser(username: string, value : UpdateParams){ // or {firstname, lastname, password} : UpdateParams
     const res = await prisma.user.update({
         where: {
             username: username
@@ -34,7 +42,7 @@ async function updateTodo(username: string, value : UpdateParams){ // or {firstn
     console.log(res)
 }
 
-async function deleteTodo(username: string){
+async function deleteUser(username: string){
     const res = await prisma.user.delete({
         where: {
             username
@@ -43,10 +51,83 @@ async function deleteTodo(username: string){
     console.log(res)
 }
 
-async function getTodo(username: string){
+async function getUser(username: string){
     const res = await prisma.user.findFirst({
         where: {
             username
+        }
+    })
+    console.log(res)
+}
+
+// -------------------------------------------------------------- Todo --------------------------------------------------------------
+
+// insertTodo("First Todo", "This is the first todo", false, 1)
+// insertTodo("Second Todo", "This is the second todo", true, 1)
+// insertTodo("Third Todo", "This is the third todo", false, 4)
+// insertTodo("Fourth Todo", "This is the fourth todo", false, 2)
+// insertTodo("Fifth Todo", "This is the fifth todo", false, 3)
+// insertTodo("Sixth Todo", "This is the sixth todo", false, 3)
+
+// getAllTodo()
+
+async function insertTodo(title: string, description: string, done: boolean, userId: number){
+    const res = await prisma.todos.create({
+        data: {
+            title,
+            description,
+            done,
+            userId
+        }
+    })
+    console.log(res)
+}
+
+interface UpdateParams {
+    description?: string,
+    done?: boolean,
+}
+
+async function updateTodo(title: string, value : UpdateParams){ // or {firstname, lastname, password} : UpdateParams
+    const res = await prisma.todos.update({
+        where: {
+            title: title
+        },
+        data: {
+            description: value.description,
+            done: value.done
+        }
+    })
+    console.log(res)
+}
+
+async function deleteTodo(title: string){
+    const res = await prisma.todos.delete({
+        where: {
+            title
+        }
+    })
+    console.log(res)
+}
+
+async function getTodo(title: string){
+    const res = await prisma.todos.findMany({
+        where: {
+            title
+        }
+    })
+    console.log(res)
+}
+
+async function getAllTodo(){
+    const res = await prisma.todos.findMany()
+    console.log(res)
+}
+
+async function getUserAllTodos(id: number){
+    const res = await prisma.todos.findMany({
+        where: {
+            userId: id
         }
     })
     console.log(res)
